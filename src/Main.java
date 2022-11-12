547e1c36230149ef4732cbb0f408e7ac79c758f8
Bei <bei.jia@dcmail.ca>
2022-11-12 00:03:32 -0500

    converted Config to Singleton

--- src/Main.java
+++ src/Main.java
@@ -2,54 +2,25 @@ import java.util.Scanner;
 
 public class Main {
 
-    /**
-     * This is a generic Console Input Function that gets input from the keyboard and
-     * saves the data to an element of an array.
-     * @param prompt { String}
-     * @param object {T[]}
-     * @return
-     * @param <T>
-     */
-   public static <T> T getConsoleInputToArray(String prompt, T[] object)
-   {
-       Scanner console=new Scanner(System.in);
-       System.out.print(prompt);
+    public static void main(String[] args)
+    {
+        String[] myStrings = new String[Console.Instance().NUM_OF_STRINGS]; // new empty container of type String
+        Integer[] myInts = new Integer[Console.Instance().NUM_OF_INTEGERS];
 
-       switch (object.getClass().getSimpleName())
-       {
-           case "Double[]":
-               return (T) ((Double)console.nextDouble());
-           case "Integer[]":
-               return (T) ((Integer)console.nextInt());
-           case "Float[]":
-               return (T) ((Float)console.nextFloat());
-           default:
-               return (T) console.nextLine();
-       }
-   }
+        for (int i = 0; i < Console.Instance().NUM_OF_STRINGS; i++)
+        {
+            myStrings[i] = Console.Instance().getConsoleInputToArray("Please enter your name: ", myStrings);
+        }
 
-       public static <T> void displayArray(T[] array)
-   {
-       for(var element: array)
-       {
-           System.out.println(element);
-       }
-   }
+        Console.Instance().displayArray(myStrings);
 
-   public static void main(String[] args)
-   {
-       String[] myStrings=new String[Config.NUM_OF_STRINGS]; //new empty container of type string
-       Integer[] myInts=new Integer[Config.NUM_OF_INTEGERS];
-       for (int i=0; i<Config.NUM_OF_STRINGS;i++)
-       {
-           myStrings[i]=getConsoleInputToArray("Please enter your name: ",myStrings );
-       }
-       displayArray(myStrings);
+        for (int i = 0; i < Console.Instance().NUM_OF_INTEGERS; i++)
+        {
+            myInts[i] = Console.Instance().getConsoleInputToArray("Please enter your number: ", myInts);
+        }
 
-       for (int i=0; i<Config.NUM_OF_INTEGERS;i++)
-       {
-           myInts[i]=getConsoleInputToArray("Please enter your number: ",myInts );
-       }
-       displayArray(myInts);
-   }
+        Console.Instance().displayArray(myInts);
+
+
+    }
 }
